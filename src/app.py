f6ab85163859a0f0701b01c4f0a81ed1ee2ed236
Ricenquimac <117040339+Ricenquimac@users.noreply.github.com>
2025-01-14 15:48:13 +0000

    MLPROJECT

--- src/app.py
+++ src/app.py
@@ -2,3 +2,112 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+import pandas as pd
+
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+total_data.head()
+
+total_data.to_csv("../data/raw/total_data.csv", index = False)
+total_data.shape
+total_data.info()
+
+id_duplicates = total_data['id'].duplicated().sum()
+print(f"The id duplicates is: {id_duplicates}")
+name_duplicates = total_data['name'].duplicated().sum()
+print(f"The name duplicates is: {name_duplicates}")
+hostid_duplicates = total_data['host_id'].duplicated().sum()
+print(f"The host id duplicates is: {hostid_duplicates}")
+
+total_data.drop(["id", "name", "host_name", "last_review", "reviews_per_month"], axis = 1, inplace = True)
+total_data.head()
+
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+fig, axis = plt.subplots(2, 3, figsize=(15, 7))
+
+sns.histplot(ax = axis[0,0], data = total_data, x = "host_id")
+sns.histplot(ax = axis[0,1], data = total_data, x = "neighbourhood_group").set_xticks([])
+sns.histplot(ax = axis[0,2], data = total_data, x = "neighbourhood").set_xticks([])
+sns.histplot(ax = axis[1,0], data = total_data, x = "room_type")
+sns.histplot(ax = axis[1,1], data = total_data, x = "availability_365")
+fig.delaxes(axis[1, 2])
+
+plt.tight_layout()
+plt.show()
+
+fig, axis = plt.subplots(4, 2, figsize = (10, 14), gridspec_kw = {"height_ratios": [6, 1, 6, 1]})
+
+sns.histplot(ax = axis[0, 0], data = total_data, x = "price")
+sns.boxplot(ax = axis[1, 0], data = total_data, x = "price")
+
+sns.histplot(ax = axis[0, 1], data = total_data, x = "minimum_nights").set_xlim(0, 200)
+sns.boxplot(ax = axis[1, 1], data = total_data, x = "minimum_nights")
+
+sns.histplot(ax = axis[2, 0], data = total_data, x = "number_of_reviews")
+sns.boxplot(ax = axis[3, 0], data = total_data, x = "number_of_reviews")
+
+sns.histplot(ax = axis[2,1], data = total_data, x = "calculated_host_listings_count")
+sns.boxplot(ax = axis[3, 1], data = total_data, x = "calculated_host_listings_count")
+
+plt.tight_layout()
+plt.show()
+
+#Numerical-Numerical Analysis
+fig, axis = plt.subplots(4, 2, figsize = (10, 16))
+
+sns.regplot(ax = axis[0, 0], data = total_data, x = "minimum_nights", y = "price")
+sns.heatmap(total_data[["price", "minimum_nights"]].corr(), annot = True, fmt = ".2f", ax = axis[1, 0], cbar = False)
+
+sns.regplot(ax = axis[0, 1], data = total_data, x = "number_of_reviews", y = "price").set(ylabel = None)
+sns.heatmap(total_data[["price", "number_of_reviews"]].corr(), annot = True, fmt = ".2f", ax = axis[1, 1])
+
+sns.regplot(ax = axis[2, 0], data = total_data, x = "calculated_host_listings_count", y = "price").set(ylabel = None)
+sns.heatmap(total_data[["price", "calculated_host_listings_count"]].corr(), annot = True, fmt = ".2f", ax = axis[3, 0]).set(ylabel = None)
+fig.delaxes(axis[2, 1])
+fig.delaxes(axis[3, 1])
+
+plt.tight_layout()
+plt.show()
+
+#Categorical-Categorical Analysis
+
+fig, axis = plt.subplots(figsize = (5, 4))
+
+sns.countplot(data = total_data, x = "room_type", hue = "neighbourhood_group")
+
+plt.show()
+
+#Numerical-Categorical Analysis
+
+total_data["room_type"] = pd.factorize(total_data["room_type"])[0]
+total_data["neighbourhood_group"] = pd.factorize(total_data["neighbourhood_group"])[0]
+total_data["neighbourhood"] = pd.factorize(total_data["neighbourhood"])[0]
+
+fig, axes = plt.subplots(figsize=(15, 15))
+
+sns.heatmap(total_data[["neighbourhood_group", "neighbourhood", "room_type", "price", "minimum_nights",	
+                        "number_of_reviews", "calculated_host_listings_count", "availability_365"]].corr(), annot = True, fmt = ".2f")
+
+plt.tight_layout()
+plt.show()
+
+#All Data Analysis
+sns.pairplot(data = total_data)
+total_data.describe()
+
+fig, axes = plt.subplots(3, 3, figsize = (15, 15))
+
+sns.boxplot(ax = axes[0, 0], data = total_data, y = "neighbourhood_group")
+sns.boxplot(ax = axes[0, 1], data = total_data, y = "price")
+sns.boxplot(ax = axes[0, 2], data = total_data, y = "minimum_nights")
+sns.boxplot(ax = axes[1, 0], data = total_data, y = "number_of_reviews")
+sns.boxplot(ax = axes[1, 1], data = total_data, y = "calculated_host_listings_count")
+sns.boxplot(ax = axes[1, 2], data = total_data, y = "availability_365")
+sns.boxplot(ax = axes[2, 0], data = total_data, y = "room_type")
+
+plt.tight_layout()
+plt.show()
+
+price_stats = total_data["price"].describe()
+price_stats
